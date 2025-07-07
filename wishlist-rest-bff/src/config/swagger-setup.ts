1056de9948b17f665f0cc27899065c0412be07cf
guilherme.goncalves <guilherme_marciano@outlook.com.br>
2025-07-07 14:05:20 -0300

    chore(wishlist-rest-bff): add swagger documentation

---
+++ wishlist-rest-bff/src/config/swagger-setup.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Wishlist REST BFF')
+    .setDescription('Wishlist REST Backend for Frontend API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-ui', app, document);
+}
